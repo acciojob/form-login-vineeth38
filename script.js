dcf241fafbfed691c94eff718bbe77961c60dc9d
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-29 22:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function getFormvalue() {
     //Write your code here
- const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  alert(`${firstName} ${lastName}`);
+ var x=document.getElementById("form1");
+    let name = ""
+    for (var i=0;i<x.length;i++) {
+        if (x.elements[i].value!='Submit')
+            name += x.elements[i].value + " "
+    }
+    alert(name.substring(0,name.length-1));
 }
