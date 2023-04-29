375faacb5eb5ce19172d4ed9e466752626846370
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-29 22:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+ const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  alert(`${firstName} ${lastName}`);
 }
